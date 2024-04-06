3d72a510a94088ceb10dd1af5f6c5c176c86b8f6
EugeneGridnev <eugenegrid@yandex.ru>
2024-04-06 20:49:56 +0300

    initial fragments logic

--- app/src/main/java/ru/eugeneprojects/avitofilms/presentation/fragments/MovieFragment.kt
+++ app/src/main/java/ru/eugeneprojects/avitofilms/presentation/fragments/MovieFragment.kt
@@ -1,4 +1,32 @@
 package ru.eugeneprojects.avitofilms.presentation.fragments
 
-class MovieFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.eugeneprojects.avitofilms.databinding.FragmentMovieBinding
+
+class MovieFragment : Fragment() {
+    private var binding: FragmentMovieBinding? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMovieBinding.inflate(inflater)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
+    }
 }
--- app/src/main/java/ru/eugeneprojects/avitofilms/presentation/fragments/MoviesListFragment.kt
+++ app/src/main/java/ru/eugeneprojects/avitofilms/presentation/fragments/MoviesListFragment.kt
@@ -1,4 +1,30 @@
 package ru.eugeneprojects.avitofilms.presentation.fragments
 
-class MoviesListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.eugeneprojects.avitofilms.databinding.FragmentMoviesListBinding
+
+class MoviesListFragment : Fragment() {
+    private var binding: FragmentMoviesListBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMoviesListBinding.inflate(inflater)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
+    }
 }
