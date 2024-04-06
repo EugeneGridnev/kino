093a058185cc5038e335f0e5bbff6f2aa46e0082
EugeneGridnev <eugenegrid@yandex.ru>
2024-04-06 23:15:10 +0300

    initial api added

---
+++ app/src/main/java/ru/eugeneprojects/avitofilms/api/MoviesAPI.kt
@@ -0,0 +1,19 @@
+package ru.eugeneprojects.avitofilms.api
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+import ru.eugeneprojects.avitofilms.BuildConfig
+import ru.eugeneprojects.avitofilms.data.models.MoviesResponse
+
+interface MoviesAPI {
+
+    @GET("/1.4/movie/search")
+    suspend fun getMovies(
+        @Query("q")
+        searchText: String = "",
+        @Header("X-API-KEY")
+        apiKey: String = BuildConfig.API_KEY
+    ): Response<MoviesResponse>
+}
