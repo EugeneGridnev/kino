4bd36c49c1bd6f87d63a5cdf6e5032228969c434
EugeneGridnev <eugenegrid@yandex.ru>
2024-04-08 14:44:31 +0300

    bottom sheet for full comment

---
+++ app/src/main/java/ru/eugeneprojects/avitofilms/presentation/fragments/CommentBottomSheetFragment.kt
@@ -0,0 +1,6 @@
+package ru.eugeneprojects.avitofilms.presentation.fragments
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class CommentBottomSheetFragment : BottomSheetDialogFragment() {
+}
