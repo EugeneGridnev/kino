313e9a332f122bfd19e51d3e413833be996e3521
EugeneGridnev <eugenegrid@yandex.ru>
2024-04-08 16:25:51 +0300

    small model fix

--- app/src/main/java/ru/eugeneprojects/avitofilms/data/models/Movie.kt
+++ app/src/main/java/ru/eugeneprojects/avitofilms/data/models/Movie.kt
@@ -3,16 +3,12 @@ package ru.eugeneprojects.avitofilms.data.models
 data class Movie(
     val ageRating: Int,
     val alternativeName: String?,
-    val budget: Budget?,
     val countries: List<Country>,
-    val description: String,
     val enName: String?,
     val genres: List<Genre>,
     val id: Int,
-    val movieLength: Int,
     val name: String?,
     val poster: Poster,
     val rating: Rating,
-    val slogan: String?,
     val year: Int
 )
