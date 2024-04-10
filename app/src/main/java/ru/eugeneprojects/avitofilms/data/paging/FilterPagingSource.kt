13aa8b3a852d3ae162df5216d307d8d5e87ff48b
EugeneGridnev <eugenegrid@yandex.ru>
2024-04-10 15:00:20 +0300

    filtered movie list paging source

--- app/src/main/java/ru/eugeneprojects/avitofilms/data/paging/FilterPagingSource.kt
+++ app/src/main/java/ru/eugeneprojects/avitofilms/data/paging/FilterPagingSource.kt
@@ -1,8 +1,8 @@
 package ru.eugeneprojects.avitofilms.data.paging
 
 import retrofit2.Response
-import ru.eugeneprojects.avitofilms.data.models.Movie
-import ru.eugeneprojects.avitofilms.data.models.PageResponse
+import ru.eugeneprojects.avitofilms.data.models.movieCardItem.Movie
+import ru.eugeneprojects.avitofilms.data.models.movieCardItem.PageResponse
 import ru.eugeneprojects.avitofilms.data.models.filters.MovieFilters
 import ru.eugeneprojects.avitofilms.data.network.repository.MoviesRepository
 
